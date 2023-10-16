5e49884fc3bd736a3453979e0123b1bcbcff6011
AleksanderWWW <alwojnarowicz@gmail.com>
2023-10-16 15:45:58 +0200

    refactor

--- src/abcd_graph_generator/graph_sampler.py
+++ src/abcd_graph_generator/graph_sampler.py
@@ -3,56 +3,56 @@ from typing import Set
 
 import numpy as np
 
+import abcd_graph_generator.utils as utils
 from abcd_graph_generator.abcd_params import ABCDParams
-from abcd_graph_generator.utils import get_mul
 
 
 def populate_clusters(params: ABCDParams) -> np.ndarray:
-    mul = get_mul(params)
+    tabu_set: Set[int] = set()
 
-    slots = copy.deepcopy(params.s)
+    mul = utils.get_mul(params)
+
+    slots = copy.copy(params.s)
     clusters = np.ones(len(params.w)) * (-1)
 
     if params.has_outliers:
-        nout = params.s[0]
-        n = len(params.w)
-        L = sum(map(lambda d: min(1.0, params.xi * d), params.w))
-        threshold = L + nout - L * nout / n - 1.0
-        idx = np.searchsorted(params.w, threshold, side="right")  # TODO to be confirmed
-
-        if len(params.w[idx:]) < nout:
-            raise ValueError("not enough nodes feasible for classification as outliers")
-
-        tabu = np.random.choice(range(idx, n), nout, replace=False)  # TODO n or n+1?
+        tabu = utils.get_tabu(params)
         clusters[tabu] = 1
         slots[1] = 0
-        stabu = set(tabu)
-
-    else:
-        stabu: Set[int] = set()
+        tabu_set = set(tabu)
 
     # TODO wtf??
     j0 = int(params.has_outliers)
-    j = j0
-    for (i, vw) in enumerate(params.w):
-        if i not in stabu:
+    j = copy.copy(j0)
+    for i, weight in enumerate(params.w):
+        if i not in tabu_set:
             continue
 
-        while j + 1 <= len(params.s) and mul * vw + 1 <= params.s[j + 1]:
+        while j + 1 <= len(params.s) and mul * weight + 1 <= params.s[j + 1]:
             j += 1
 
         if j != j0:
-            raise Exception(f"could not find a large enough cluster for vertex of weight {vw}")
+            raise ValueError(f"Could not find a large enough cluster for vertex of weight {weight}.")
 
-        wts = np.array(slots[j0 + 1 : j])
+        wts = slots[j0 + 1 : j]
 
         if wts.sum() != 0:
-            raise Exception(f"could not find an empty slot for vertex of weight {vw}")
+            raise ValueError(f"Could not find an empty slot for vertex of weight {weight}.")
 
-        loc = np.random.choice(range(j0 + 1, j), wts)
+        loc = np.random.choice(range(j0 + 1, j + 1), wts)
         clusters[i] = loc
         slots[loc] -= 1
 
-    assert sum(slots) == 0
-    assert min(clusters) == 1
+    _raise_for_values(slots, clusters)
+
     return clusters
+
+
+def _raise_for_values(slots: np.ndarray, clusters: np.ndarray) -> None:
+    slots_sum = slots.sum()
+    if slots_sum != 0:
+        raise ValueError(f"Slots sum expected to be 0. Actual: {slots_sum}.")
+
+    min_clusters = np.min(clusters)
+    if min_clusters != 1:
+        raise ValueError(f"Cluster min expected to be 1. Actual: {min_clusters}.")
--- src/abcd_graph_generator/utils.py
+++ src/abcd_graph_generator/utils.py
@@ -1,5 +1,7 @@
 __all__ = [
     "get_mul",
+    "get_tabu",
+    "randround",
 ]
 
 """Module to host helper functions for calculations."""
@@ -18,11 +20,24 @@ def get_mul(params: ABCDParams) -> float:
     n = len(params.w)
     if params.has_outliers:
         s0 = params.s[0]
-        phi = 1.0 - sum((sl / (n - s0)) ^ 2 for sl in params.s[2:]) * (n - s0) * params.xi / ((n - s0) * params.xi + s0)
+        phi = 1 - sum((sl / (n - s0)) ** 2 for sl in params.s[2:]) * (n - s0) * params.xi / ((n - s0) * params.xi + s0)
     else:
-        phi = 1.0 - sum((sl / n) ^ 2 for sl in params.s)
+        phi = 1 - sum((sl / n) ^ 2 for sl in params.s)
 
     return 1.0 - params.xi * phi
+
+
+def get_tabu(params: ABCDParams) -> np.ndarray:
+    nout = params.s[0]
+    n = len(params.w)
+    L = sum(map(lambda d: min(1.0, params.xi * d), params.w))
+    threshold = L + nout - L * nout / n - 1.0
+    idx = np.argmax(params.w <= threshold)  # index of the first elem of w that is less or equal to the threshold
+
+    if len(params.w[idx:]) < nout:
+        raise ValueError("Not enough nodes feasible for classification as outliers")
+
+    return np.random.choice(range(idx, n + 1), nout, replace=False)
 
 
 def randround(x: Union[float, int]) -> int:
