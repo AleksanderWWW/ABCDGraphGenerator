f8a4bf7d35ce477fd0f9e13d3f997c9c3beab143
AleksanderWWW <alwojnarowicz@gmail.com>
2023-10-18 11:00:05 +0200

    implement CL model

--- src/abcd_graph_generator/pl_sampler.py
+++ src/abcd_graph_generator/pl_sampler.py
@@ -7,7 +7,7 @@ import numpy as np
 
 def trunc_powerlaw_weights(alpha: float, v_min: int, v_max: int) -> np.ndarray[float]:
     if alpha < 1:
-        raise ValueError(f"Parameter 'alpha' should be greater or equal to 1. Got: {alpha}.")
+        raise ValueError(f"Parameter 'alpha' should be greater than or equal to 1. Got: {alpha}.")
 
     if v_min < 1 or v_min > v_max:
         raise ValueError(f"Parameter 'v_min' should not be smaller than 1 or greater than {v_max}.")
